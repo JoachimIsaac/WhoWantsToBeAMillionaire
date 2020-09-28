1595e7bb0bcf7b6e285222c70334838c70c60466
Joachim Isaac <joachimvivian3@gmail.com>
2020-09-28 13:47:53 -0500

    Update Program.cs

--- WhoWantsToBeAMillionaire/Program.cs
+++ WhoWantsToBeAMillionaire/Program.cs
@@ -5,6 +5,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ 
+Names: Joachim Isaac 
+Date:09/28/2020
+
+Course: CMPS4143, Fall 2020, Dr. Stringfellow.
+
+
+Purpose: To implement a GUI of the game who wants to be a millioniare.
+
+ */
+
 
 namespace WhoWantsToBeAMillionaire
 {
@@ -17,18 +29,11 @@ namespace WhoWantsToBeAMillionaire
         static void Main()
         {
 
-
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Infile f = new Infile();
             Application.Run(f);
-
-
-
-            
-
-
 
         }
     }
